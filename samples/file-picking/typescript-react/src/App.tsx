0e06d7be6caf1801af9b445d6c4f9ed6cb814c7d
shoma <koalakoala.xxx@gmail.com>
2023-06-03 21:44:59 +0900

    commit for moving repo

--- samples/file-picking/typescript-react/src/App.tsx
+++ samples/file-picking/typescript-react/src/App.tsx
@@ -1,35 +1,34 @@
 import React, { useState } from 'react';
 import './App.css';
-import { getToken } from "./auth";
+import { getToken } from './auth';
 import PickerButton from './components/picker';
-import { IFilePickerOptions } from "@pnp/picker-api";
-import PickedFilesList from "./components/picked-files-list";
+import { IFilePickerOptions } from '@pnp/picker-api';
+import PickedFilesList from './components/picked-files-list';
 
 const paramsTest: IFilePickerOptions = {
-  sdk: "8.0",
+  sdk: '8.0',
   entry: {
-    oneDrive: {}
+    oneDrive: {},
   },
   authentication: {},
   messaging: {
-    origin: "http://localhost:3000",
-    channelId: "27"
+    origin: 'http://localhost:3000',
+    channelId: '27',
   },
   selection: {
-    mode: "multiple",
+    mode: 'multiple',
   },
   typesAndSources: {
     // filters: [".docx"],
-    mode: "files",
+    mode: 'files',
     pivots: {
       oneDrive: true,
       recent: true,
-    }
-  }
+    },
+  },
 };
 
 function App() {
-
   const [results, setResults] = useState(null);
 
   function onPicked(pickerResults) {
@@ -38,11 +37,15 @@ function App() {
     }
   }
 
-
   return (
     <div className="App">
       Launch the picker using this button: <br />
-      <PickerButton baseUrl="https://{tenant}-my.sharepoint.com/" getToken={getToken} options={paramsTest} onResults={onPicked} />
+      <PickerButton
+        baseUrl="https://api.onedrive.com/"
+        getToken={getToken}
+        options={paramsTest}
+        onResults={onPicked}
+      />
       <PickedFilesList items={results} />
     </div>
   );
--- samples/file-picking/typescript-react/src/auth.ts
+++ samples/file-picking/typescript-react/src/auth.ts
@@ -1,50 +1,50 @@
-import { PublicClientApplication, Configuration, SilentRequest } from "@azure/msal-browser";
-import { combine } from "@pnp/core";
-import { IAuthenticateCommand } from "@pnp/picker-api/dist";
+import {
+  PublicClientApplication,
+  Configuration,
+  SilentRequest,
+} from '@azure/msal-browser';
+import { combine } from '@pnp/core';
+import { IAuthenticateCommand } from '@pnp/picker-api/dist';
 
 const msalParams: Configuration = {
-    auth: {
-        authority: "https://login.microsoftonline.com/{tenant id / common}",
-        clientId: "{client id}",
-        redirectUri: "http://localhost:3000"
-    },
-}
+  auth: {
+    authority:
+      'https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a',
+    clientId: '86fe9fe1-b505-43b4-af3f-d4a4f11404f4',
+    redirectUri: 'http://localhost:3000',
+  },
+};
 
 const app = new PublicClientApplication(msalParams);
 
 export async function getToken(command: IAuthenticateCommand): Promise<string> {
-
-    return getTokenWithScopes([`${combine(command.resource, ".default")}`]);
+  return getTokenWithScopes([`${combine(command.resource, '.default')}`]);
 }
 
-export async function getTokenWithScopes(scopes: string[], additionalAuthParams?: Omit<SilentRequest, "scopes">): Promise<string> {
+export async function getTokenWithScopes(
+  scopes: string[],
+  additionalAuthParams?: Omit<SilentRequest, 'scopes'>
+): Promise<string> {
+  let accessToken = '';
+  const authParams = { scopes, ...additionalAuthParams };
 
-    let accessToken = "";
-    const authParams = { scopes, ...additionalAuthParams };
+  try {
+    // see if we have already the idtoken saved
+    const resp = await app.acquireTokenSilent(authParams!);
+    accessToken = resp.accessToken;
+  } catch (e) {
+    // per examples we fall back to popup
+    const resp = await app.loginPopup(authParams!);
+    app.setActiveAccount(resp.account);
 
-    try {
-
-        // see if we have already the idtoken saved
-        const resp = await app.acquireTokenSilent(authParams!);
-        accessToken = resp.accessToken;
-
-    } catch (e) {
-
-        // per examples we fall back to popup
-        const resp = await app.loginPopup(authParams!);
-        app.setActiveAccount(resp.account);
-
-        if (resp.idToken) {
-
-            const resp2 = await app.acquireTokenSilent(authParams!);
-            accessToken = resp2.accessToken;
-
-        } else {
-
-            // throw the error that brought us here
-            throw e;
-        }
+    if (resp.idToken) {
+      const resp2 = await app.acquireTokenSilent(authParams!);
+      accessToken = resp2.accessToken;
+    } else {
+      // throw the error that brought us here
+      throw e;
     }
+  }
 
-    return accessToken;
+  return accessToken;
 }
